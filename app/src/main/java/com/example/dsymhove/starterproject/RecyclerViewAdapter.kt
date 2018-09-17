d94edce47148f4ac342f7d35269cdbfd7e37b80a
dsymhove <david.symhoven@jambit.com>
2018-09-17 10:04:58 +0200

    finishes implementing recycler view adapter

--- app/src/main/java/com/example/dsymhove/starterproject/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/dsymhove/starterproject/RecyclerViewAdapter.kt
@@ -1,24 +1,33 @@
 package com.example.dsymhove.starterproject
 
 import android.support.v7.widget.RecyclerView
+import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
+import org.w3c.dom.Text
 
-class RecyclerViewAdapter(private val dataset: Array<String>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
-
+class RecyclerViewAdapter(): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
-        val textView = LayoutInflater.from(parent.context) as TextView
-        return ViewHolder(textView)
-
-    }
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.layout_list_item, parent, false) as TextView
+        return ViewHolder(rootView)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 5
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d("tag", "onBindViewHolder called")
+
+        holder.textView.text = "This is my first Recycler View"
+    }
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {
+
     }
 }
