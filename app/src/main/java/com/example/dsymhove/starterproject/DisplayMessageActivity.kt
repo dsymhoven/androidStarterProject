1e6c95f9ca28eda0663c0fa94ead83480580e070
dsymhove <david.symhoven@jambit.com>
2018-09-17 10:22:09 +0200

    makes recycler view work

--- app/src/main/java/com/example/dsymhove/starterproject/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/dsymhove/starterproject/DisplayMessageActivity.kt
@@ -2,6 +2,9 @@ package com.example.dsymhove.starterproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
@@ -15,5 +18,15 @@ class DisplayMessageActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView2).apply {
             text = message
         }
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        Log.d("tag", "init recycler view")
+        val recyclerView = findViewById<RecyclerView>(R.id.myRecyclerView)
+        val recyclerViewAdapter = RecyclerViewAdapter()
+        recyclerView.adapter = recyclerViewAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/example/dsymhove/starterproject/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/dsymhove/starterproject/RecyclerViewAdapter.kt
@@ -9,25 +9,26 @@ import android.view.ViewGroup
 import android.widget.RelativeLayout
 import android.widget.TextView
 import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.layout_list_item.view.*
 import org.w3c.dom.Text
 
 class RecyclerViewAdapter(): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
-        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.layout_list_item, parent, false) as TextView
+        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.layout_list_item, parent, false)
         return ViewHolder(rootView)
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return 8
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.d("tag", "onBindViewHolder called")
 
-        holder.textView.text = "This is my first Recycler View"
+        holder.textView.text = "This is my first RecyclerView"
     }
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {
-
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView = view.myTextView
     }
 }
