7d96d6b74a3f324a915a1e80c8c7ec514f912ce1
dsymhove <david.symhoven@jambit.com>
2018-09-27 09:59:09 +0200

    adds firebase storage reference does not work yet

--- app/src/main/java/com/example/dsymhove/starterproject/Tab4Fragment.kt
+++ app/src/main/java/com/example/dsymhove/starterproject/Tab4Fragment.kt
@@ -1,6 +1,7 @@
 package com.example.dsymhove.starterproject
 
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -13,6 +14,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 /**
  * A simple [Fragment] subclass.
@@ -22,6 +24,7 @@ class Tab4Fragment : Fragment() {
 
     private var profiles : MutableList<Profile> = mutableListOf()
     private lateinit var recyclerView: RecyclerView
+    private val ONE_MEGABYTE: Long = 2024 * 2024
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -30,7 +33,15 @@ class Tab4Fragment : Fragment() {
         recyclerView = rootView.findViewById<RecyclerView>(R.id.profile_recycler_view)
         val database = FirebaseDatabase.getInstance()
         val reference = database.getReference("users")
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.getReferenceFromUrl("gs://jambit-academy-7ea09.appspot.com/astefano.png")
+
         reference.orderByKey().addValueEventListener(itemListener)
+
+        storageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
+            val bmp = BitmapFactory.decodeByteArray(bytes,0, bytes.size)
+
+        }
         return rootView
     }
 
